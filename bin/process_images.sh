a4ff66abea2b120bed246d90bdf085cdc7ef4308
Patrick Farrell <mr.patfarrell@gmail.com>
2024-12-17 15:20:49 -0500

    safer file discovery

--- bin/process_images.sh
+++ bin/process_images.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-for src in ../*.jpg ../*.jpeg ../*.JPG ../*.JPEG;
-  do
-    cropped=$(echo $src | sed -e 's/\.\.\///');
-    if [ ! -f $cropped ]; then
-      smartcrop -W 295 -H 177 -i $src -o $cropped;
+for src in ../*.jpg ../*.jpeg ../*.JPG ../*.JPEG; do
+    if [ -f "$src" ]; then
+        cropped=$(echo $src | sed -e 's/\.\.\///');
+        if [ ! -f $cropped ]; then
+            smartcroppy --width 295 --height 177 $src $cropped;
+        fi;
     fi;
-  done
+done
+
